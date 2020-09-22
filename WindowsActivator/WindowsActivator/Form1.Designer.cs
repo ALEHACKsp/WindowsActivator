cffa295260c879a7156f8e35ef3bef9c876305a5
Strayfade <64668046+StrayfadeGH@users.noreply.github.com>
2020-09-22 09:38:56 -0400

    Added 1.5.1

--- WindowsActivator/WindowsActivator/Form1.Designer.cs
+++ WindowsActivator/WindowsActivator/Form1.Designer.cs
@@ -440,6 +440,7 @@
             this.Controls.Add(this.groupBox1);
             this.ForeColor = System.Drawing.Color.Black;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.MaximumSize = new System.Drawing.Size(816, 489);
             this.MinimumSize = new System.Drawing.Size(816, 325);
             this.Name = "Form1";

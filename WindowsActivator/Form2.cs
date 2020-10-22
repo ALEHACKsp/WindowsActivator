ec64f49d30b97f7087aa65bed39ad15b7504b485
Strayfade <64668046+Strayfade@users.noreply.github.com>
2020-10-22 10:38:32 -0400

    Add files via upload

---
+++ WindowsActivator/Form2.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace WindowsActivator
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        private async void FadeIn(Form o, int interval = 80)
+        {
+            while (o.Opacity < 1.0)
+            {
+                await Task.Delay(interval);
+                o.Opacity += 0.05;
+            }
+            o.Opacity = 1;     
+        }
+
+        private async void FadeOut(Form o, int interval = 80)
+        {
+            while (o.Opacity > 0.0)
+            {
+                await Task.Delay(interval);
+                o.Opacity -= 0.05;
+            }
+            o.Opacity = 0; 
+        }
+        private void label2_Click(object sender, EventArgs e)
+        {
+            Thread.Sleep(1000);
+            FadeOut(this, 1000);
+        }
+        private void Splash_Load(object sender, EventArgs e)
+        {
+            this.Opacity = 1.0;
+            FadeIn(this, 70);
+        }
+
+
+        private void Splash_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            FadeOut(this, 30);
+        }
+    }
+}
---
+++ WindowsActivator/Resources/test.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsActivator.Resources
+{
+    class test
+    {
+        int two = 1;
+        string constr = '1 lo' + 0.ToString() + 'v3 y' + '0u'.ToString();
+    }
+}

7f0356af8960aa7737de53c29f37b0026795b116
Strayfade <64668046+Strayfade@users.noreply.github.com>
2020-11-10 15:11:32 -0500

    Started 3.1

--- WindowsActivator3/Form1.cs
+++ WindowsActivator3/Form1.cs
@@ -1,15 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using WindowsActivator3.Properties;
-using OpenHardwareMonitor.Hardware;
-using System.Timers;
 using System.Diagnostics;
 using System.Threading;
 using Microsoft.Win32;
@@ -18,7 +10,7 @@ namespace WindowsActivator3
 {
     public partial class Form1 : Form
     {
-        string version = "3.0";
+        string version = "3.1";
         string buildDate = "11/10/2020";
 
         string trueVersion;
